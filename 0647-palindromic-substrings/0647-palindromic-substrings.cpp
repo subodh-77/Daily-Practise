8676915ff5f6992abf7ed750f71f91d77d4d81ce
Subodh Pathak <148983459+subodh-77@users.noreply.github.com>
2026-07-02 11:16:06 +0530

    Enhance palindrome checking and counting logic
    Updated the is_palin function for improved memoization and added logic to count palindromic substrings.

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -17,3 +17,22 @@ public:
         return count;
     }
 };
+//update is_palin function for further memoization
+class Solution {
+public:
+    bool is_palin(string &s , int i ,int j){
+        if(i>j)return true;
+        if(s[i]==s[j]) return is_palin(s,i+1,j-1);
+        return false;
+    }
+    int countSubstrings(string s) {
+        int n = s.length();
+        int count = 0;
+        for(int i = 0;i<n;i++){
+            for(int j = i;j<n;j++){
+                if(is_palin(s,i,j))count++;
+            }
+        }
+        return count;
+    }
+};
