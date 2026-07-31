7e3f52b6418e8ae79d2bfe79f818427a7f61bb19
Subodh Pathak <148983459+subodh-77@users.noreply.github.com>
2026-07-31 11:53:57 +0530

    Time: 9 ms (53.64%), Space: 25.4 MB (72.47%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int>mp(26,0);
+        for(char &it:word){
+            mp[it-'a']++;
+        }
+        sort(mp.begin(),mp.end(),greater<int>());
+        int result = 0;
+        for(int i = 0 ;i<26;i++){
+            int freq = mp[i];
+            int press = i/8 +1;
+            result += freq*press;
+        }
+        return result;
+    }
+};
