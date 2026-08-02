fdaa5fc5e1c5e06e44a99f06da3069e42df60ab2
Subodh Pathak <148983459+subodh-77@users.noreply.github.com>
2026-08-02 12:16:31 +0530

    Time: 7 ms (35.23%), Space: 11.4 MB (37.89%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -5,8 +5,8 @@ public:
         if(i>j)return 0;
         if(i==j)return piles[i];
         if(dp[i][j]!=-1)return dp[i][j];
-        int pick_i = piles[i]+solve(i+1,j,piles);
-        int pick_j = piles[j]+solve(i,j-1,piles);
+        int pick_i = piles[i]+min(solve(i+2,j,piles),solve(i+1,j-1,piles));
+        int pick_j = piles[j]+min(solve(i+1,j-1,piles),solve(i,j-2,piles));
 
         return dp[i][j]=max(pick_i,pick_j);
         
